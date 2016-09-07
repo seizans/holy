3571d22eee56139cb3cf1c0e2ac2f72ea445bbfa
Seizan Shimazaki <seizans@gmail.com>
2016-09-07 22:46:38 +0900

    ui/ から npm run build で app.js 作るようにする

---
+++ ui/main.js
@@ -0,0 +1,16 @@
+//import {Socket, Presence} from "./phoenix";
+
+let App = {
+  init(){
+    if(!true){ return }
+    this.connectButton = document.getElementById("connect-button")
+    this.bind()
+  },
+
+  bind(){
+      this.connectButton.addEventListener("click", e => {
+          alert("hoge")
+      })
+  },
+}
+App.init()
