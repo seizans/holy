0db5a6cd55679911f03552f8b01c275a549c7960
Seizan Shimazaki <seizans@gmail.com>
2016-09-08 14:53:21 +0900

    fetch で API 叩くまで

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,47 @@
+import 'whatwg-fetch';
 import {Socket, Presence} from "./phoenix";
 
-let App = {
-  init(){
-    if(!true){ return }
-    this.connectButton = document.getElementById("connect-button")
-    this.bind()
-  },
 
-  bind(){
-      this.connectButton.addEventListener("click", e => {
-          alert("hoge")
-      })
-  },
+let join = (user_id) => {
+    const data = new FormData();
+    data.append('user_id', user_id);
+
+    return fetch('/api/join', {
+        method: 'POST',
+        body: data
+    }).then((response) => {
+        if (response.status !== 200) {
+            const error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }
+        return response.json();
+    }).catch((error) => {
+        console.log(error);
+        return error;
+    });
+}
+
+let App = {
+    init() {
+        if(!true){ return }
+        this.connectButton = document.getElementById("connect-button")
+        this.bind()
+    },
+
+    bind() {
+        this.connectButton.addEventListener("click", e => {
+            join(1).then((json_or_error) =>{
+                if (json_or_error.response) {
+                    console.log(json_or_error);
+                    alert('error');
+                    return;
+                }
+                console.log(json_or_error.user_id);
+                return;
+            });
+        })
+    },
+
 }
 App.init()
